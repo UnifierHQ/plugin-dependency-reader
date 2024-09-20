a298a94c7110501f4e5e4a7508fad84874f2fe54
green. <41323182+greeeen-dev@users.noreply.github.com>
2024-09-20 21:18:13 +0200

    Update reader.py

--- reader.py
+++ reader.py
@@ -3,9 +3,10 @@ import sys
 
 filepath = sys.argv[len(sys.argv)-1] # use last argument as file
 
-if filepath == 'reader.py':
+if filepath == 'reader.py' or filepath.endswith('/reader.py'):
     # default to plugin.json if filepath was probably not provided
     filepath = 'plugin.json'
+    print('Reading from plugin.json as no filepath was given'
 
 with open(filepath) as file:
     data = json.load(file)
