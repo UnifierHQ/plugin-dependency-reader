6b9fba9a8aa1eaf9f9fb7594c8c2064050d4f7c5
green. <41323182+greeeen-dev@users.noreply.github.com>
2024-11-25 17:23:23 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,6 +1,11 @@
 import json
 import sys
 
+if sys.version_info.major < 3 or sys.version_info.minor < 12:
+    print('Unifier needs Python 3.12 or above to run.')
+    print('Please remove jobs using Python 3.11 or older from your workflow.')
+    sys.exit(1)
+
 filepath = sys.argv[len(sys.argv)-1] # use last argument as file
 
 if filepath == 'reader.py' or filepath.endswith('/reader.py'):
